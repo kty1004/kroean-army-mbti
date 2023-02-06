6fce831a52a737ffc3856f4ab5ed594d0813a1fa
kty1004 <ktyoung1004@gmail.com>
2023-02-06 22:48:33 +0900

    intro 페이지 버튼  hovering 수정 필요
    div의 크기는 유지되면서 background 색만 hovering에 따라 바꿔야 함.

--- src/routes/intro.js
+++ src/routes/intro.js
@@ -1,14 +1,30 @@
 import {Link} from 'react-router-dom';
+import style from '/Users/kimtaeyoung1/Documents/GitHub/kroean-army-mbti/src/styles/intro.module.css';
+import { useEffect, useState } from 'react';
 
 function Intro() {
+    const [isover, setIsOver] = useState(false);
+    
+    const hovering=()=> setIsOver(true)
+    const nonhovering=()=> setIsOver(false)
+
     return(
         <div>
             <div className="header">
                 <h1>군 생활 판독기</h1>
             </div>
             <div className="link">
-                <Link to='/typea'><h2>시작하기</h2></Link>
-                <h2>공유하기</h2>
+                <Link to='/typea'>
+                    <div onMouseEnter={hovering}
+                onMouseLeave={nonhovering}
+                className={isover? style.mouseEntering :null}
+                >시작하기</div>
+                </Link>
+                
+                <div onMouseEnter={hovering}
+                onMouseLeave={nonhovering}
+                className={isover? style.mouseEntering :null}
+                >공유하기</div>
             </div>
         </div>
     )
--- src/routes/typea.js
+++ src/routes/typea.js
@@ -3,6 +3,16 @@ function Typea(){
         <div>
             <div className="question">
                 <h4>typeA의 질문</h4>
+                <p>blablablablabla</p>
+            </div>
+
+            <div className="ox">
+                <button className="ox__o">
+                    o
+                </button>
+                <button className="ox__x">
+                    x
+                </button>
             </div>
 
         </div>
