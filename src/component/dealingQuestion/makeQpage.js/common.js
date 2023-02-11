98b1033400d9a171352e741b5df42bc133677c19
kty1004 <ktyoung1004@gmail.com>
2023-02-11 11:57:51 +0900

    readcsv.js 버그 수정함.
    rows가 가공되지 않은 상태로 return되는 현상을 해결함.

--- src/component/dealingQuestion/makeQpage.js/common.js
+++ src/component/dealingQuestion/makeQpage.js/common.js
@@ -1,19 +1,24 @@
 import {Link} from 'react-router-dom';
 import style from '/Users/kimtaeyoung1/Documents/GitHub/kroean-army-mbti/src/styles/mainpage_style/mainPageBtn.module.css';
-import SendingCsv from '/Users/kimtaeyoung1/Documents/GitHub/kroean-army-mbti/src/component/dealingQuestion/sendingCsv.js'
 
-function CommonObject(question){ //type마다 공통적으로 있는 부분
+import ReadingCsv from '/Users/kimtaeyoung1/Documents/GitHub/kroean-army-mbti/src/component/dealingQuestion/readingcsv.js';
+
+
+let reseivedData= ReadingCsv().then((res)=>reseivedData=res)
+// 비동기를 잘 이해하고 있는 것이 매우 중요하구나....
+
+function CommonObject(){ //type마다 공통적으로 있는 부분
     // 요 함수에서 질문 데이터를 받아야 함.
-    // SendingCsv()
-    const _queston =JSON.stringify(question)
+    const questionpackge=reseivedData[0]
     
+    const question ='d'
     return (
         <div>
             <Link to='/'>
                 <button className={style.goToHome}>go to Home</button>
             </Link>
             <div className="question">
-                <h4>{_queston}</h4>
+                <h4>{question}</h4>
             </div>
         </div>
     )
--- src/component/dealingQuestion/readingcsv.js
+++ src/component/dealingQuestion/readingcsv.js
@@ -1,3 +1,4 @@
+import { type } from '@testing-library/user-event/dist/type';
 import axiox from 'axios';
 
 async function ReadingCsv() {
@@ -5,19 +6,21 @@ async function ReadingCsv() {
   const data = await res.data
   const rows= data.split('\n').slice(1)
   
-
-  rows.forEach(elt => {
+  const modifedRows= rows.map(elt => {
     const modifedElt= elt.replace(/"/g, '')
     let row= modifedElt.split(',')
-
-    if (row.length >2) {
+    if (row.length >2) { //row요소가 2개 초과일때
       for (let i=2; i <row.length; i++){
         row[1]=row[1]+row[i]
       }
-      row=[row[0],row[1]]
+      row=[row[0],row[1], 'row인자 3개 였음']
+      return row
+    } else { //row요소가 정상적으로 나왔을 때
+      return row
     }
   });
-  return rows
+  
+  return modifedRows
 }
 
 export default ReadingCsv;
